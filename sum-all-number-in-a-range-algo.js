01f2204b53c512cd429c9734840ad9914aa827c0
mamun-swe <mamun.swe.277@gmail.com>
2023-04-03 02:36:59 +0600

    summ all number in a range algorithm solution added

---
+++ sum-all-number-in-a-range-algo.js
@@ -0,0 +1,16 @@
+function sumAll(arr) {
+  let num = 0;
+
+  const min = arr[0] > arr[1] ? arr[1] : arr[0];
+  const max = arr[0] < arr[1] ? arr[1] : arr[0];
+
+  for (let i = 0; i < max + 1; i++) {
+    if (i >= min) {
+      num += i;
+    }
+  }
+
+  return num;
+}
+
+console.log(sumAll([5, 10]));
