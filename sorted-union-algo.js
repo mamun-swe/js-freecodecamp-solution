cb0c34f7b20749d4db0ca4f46290785eb087b80a
mamun-swe <mamun.swe.277@gmail.com>
2023-04-04 02:12:14 +0600

    sorted union solution added

---
+++ sorted-union-algo.js
@@ -0,0 +1,17 @@
+function uniteUnique(arr) {
+  const finalArr = [];
+
+  const args = [...arguments];
+
+  for (let i = 0; i < args.length; i++) {
+    for (let k = 0; k < args[i].length; k++) {
+      if (finalArr.indexOf(args[i][k]) === -1) {
+        finalArr.push(args[i][k]);
+      }
+    }
+  }
+
+  return finalArr;
+}
+
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
