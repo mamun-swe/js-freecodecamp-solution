a4e2cc347dc77544fdf5f6621c3040e4055e6933
mamun-swe <mamun.swe.277@gmail.com>
2023-04-04 03:15:33 +0600

    streamroller solution added

---
+++ steamroller-algo.js
@@ -0,0 +1,15 @@
+function steamrollArray(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArr.push(...steamrollArray(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+const x = steamrollArray([1, [2], [3, [[4]]]]);
